f98d7f634efebbda7b95dff0a0d62587043bfcbc
fanyingfx <fanzhenxin@outlook.com>
2022-07-22 09:34:29 +0800

    patch attachbottom

--- config.h
+++ config.h
@@ -9,7 +9,7 @@ static const unsigned int snap = 32;       /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayspacing = 2;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
-static const int showsystray = 1;     /* 0 means no systray */
+static const int showsystray = 1;     /* 0 means no systray */  
 static const unsigned int gappih = 10;       /* horiz inner gap between windows */
 static const unsigned int gappiv = 10;       /* vert inner gap between windows */
 static const unsigned int gappoh = 12;       /* horiz outer gap between windows and screen edge */
@@ -82,7 +82,7 @@ static const Rule rules[] = {
 };
 
 /* layout(s) */
-static const float mfact = 0.5; /* factor of master area size [0.05..0.95] */
+static const float mfact = 0.6; /* factor of master area size [0.05..0.95] */
 static const int nmaster = 1;    /* number of clients in master area */
 static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 
